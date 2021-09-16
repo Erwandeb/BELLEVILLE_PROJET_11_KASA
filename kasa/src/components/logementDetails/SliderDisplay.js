ec6e4b5d2a06fdc9faf4f7843a3b2a1c1780601d
Erwan de Belleville <edebelleville@hotmail.fr>
2021-09-16 17:11:38 +0200

    update style

--- kasa/src/components/logementDetails/SliderDisplay.js
+++ kasa/src/components/logementDetails/SliderDisplay.js
@@ -1,15 +1,34 @@
 import {Component} from 'react'
 
 
-
-
 class SliderDisplay extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { 
+            currentImageIndex: 0
+        };
+
+
+    }
 
     render(){
 
         const { logementData } = this.props;
-      
+        console.log('logement data ? ', logementData)
+        const length = logementData.pictures.length;
+        const current = 0;
+       
+       
+        
+        const nextSlide = () => (current === length - 1 ? 0 : current + 1);
+        const prevSlide = () => (current === 0 ? length - 1 : current - 1);
+        
+        console.log("length", length)
+        console.log("current is", current)
+
+        
+
         let showImages;
         if(logementData){
             showImages = logementData.pictures.map((photo) => {
@@ -19,12 +38,17 @@ class SliderDisplay extends Component {
             showImages = "loading"
         }
         
+    
+
         
         return <div className="slider">
                     <div className="slider-window">
-                        <img src="../media/fleche-right.png" alt="fleche next" className="arrow-right"/>
-                        <img src="../media/fleche-left.png" alt="fleche preview" className="arrow-left"/>
-                        {showImages}
+                        <img src="../media/fleche-right.png" alt="fleche next" className="arrow-right" onClick={nextSlide}/>
+                        <img src="../media/fleche-left.png" alt="fleche preview" className="arrow-left" onClick={prevSlide} />
+                        <div className="slider-derouler">
+                            {showImages}
+                        </div>
+                        
                     </div>
                 </div>
     }
--- kasa/src/components/logementDetails/StarRating.js
+++ kasa/src/components/logementDetails/StarRating.js
@@ -7,8 +7,8 @@ class StarRating extends Component {
     render(){
      
         const { rating } = this.props;
-        const starRed = Array.from({length: rating}, (v, k) => <img src="../media/redStar.png" alt="etoile rouge" className="stars"/>);
-        const emptyStar = Array.from({length: 5-rating}, (v, k) => <img src="../media/emptyStar.png" alt="etoile grise" className="stars"/>);
+        const starRed = Array.from({length: rating}, (v, k) => <img src="../media/redStar.png" alt="etoile rouge" className="stars" key={rating.index}/>);
+        const emptyStar = Array.from({length: 5-rating}, (v, k) => <img src="../media/emptyStar.png" alt="etoile grise" className="stars" key={rating.index}/>);
 
         return(
             <div className="rating">
